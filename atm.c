dac4dd6428f68cfc98f52ebae51126ba917d49b1
Aimee-Gloire <i.aimeegloi@alustudent.com>
2025-06-06 09:58:49 +0200

    added user input

--- atm.c
+++ atm.c
@@ -12,8 +12,30 @@ int transactionCounts[Users] = {0};
 
 int currentUser = -1;
 
+int login(){
+    char username[20];
+    char pin[5];
+    int attempts = 0;
+
+    while (attempts < 3) {
+        printf("Enter username: ");
+        scanf("%s", username);
+        
+        printf("Enter 4-digit PIN: ");
+        scanf("%s", pin);
+
+        for (int i = 0; i < Users; i++) {
+            if (strcmp(username, usernames[i]) == 0 && strcmp(pin, pins[i]) == 0) {
+                currentUser = i;
+                printf("Login successful!\n");
+                return 1;
+            }
+        }
+}
+}
 
 int main() {
     printf("ATM Simulator\n");
+    int loginResult = login();
     return 0;
 }
